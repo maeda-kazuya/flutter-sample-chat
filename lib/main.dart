9b1b2bc265b0967091bb2244961b8cd70434cff2
Maeda, Kazuya <kazuya.maeda@gmail.com>
2019-01-20 23:08:45 +0900

    Add TextFiled and send button

--- lib/main.dart
+++ lib/main.dart
@@ -15,28 +15,58 @@ class FriendlychatApp extends StatelessWidget {
   }
 }
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return ChatScreenState();
+  }
+}
+
+class ChatScreenState extends State<ChatScreen> {
+  final _textController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("AppBar: FriendlyChat")
       ),
+      body: _buildTextComposer(),
     );
   }
-}
 
-//class MyApp extends StatelessWidget {
-//  @override
-//  Widget build(BuildContext context) {
-//    return MaterialApp(
-//      title: 'M:FriendlyChat',
-//      home: Scaffold(
-//        appBar: AppBar(
-//          title: Text('AppBar:FriendlyChat'),
-//        )
-//      )
-//    );
-//  }
-//}
-//
+  Widget _buildTextComposer() {
+    return IconTheme(
+      data: IconThemeData(
+        color: Theme.of(context).accentColor
+      ),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: <Widget>[
+            Flexible(
+              child: TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+                decoration: InputDecoration.collapsed(
+                    hintText: "Send a message"
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                icon: Icon(Icons.send),
+                onPressed: () => _handleSubmitted(_textController.text),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
+  }
+}
